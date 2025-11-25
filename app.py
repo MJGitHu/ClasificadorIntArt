7add5db4f489ffbb3f4b7cc74ea5536ace1f7cb0
MJ <mavrmaxp@outlook.com>
2025-11-25 17:07:33 -0500

    test: Ver por que no predice bien 2

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from nltk import word_tokenize, sent_tokenize
 import pandas as pd
 
 nltk.download("punkt")
+nltk.download('punkt_tab')  
+nltk.download('averaged_perceptron_tagger')
 
 # Cargar modelo y scaler
 model = joblib.load("model_logreg.pkl")
@@ -36,15 +38,19 @@ class InputText(BaseModel):
     text: str
 
 def extract_features(text):
+    text = text.strip()
+    if len(text) < 5:
+        return [0]*5
+
     try:
+        sentences = sent_tokenize(text)
         tokens = word_tokenize(text.lower())
         words = [w for w in tokens if w.isalpha()]
-        sentences = sent_tokenize(text)
 
-        if len(words) == 0 or len(sentences) == 0:
+        if len(words) < 3 or len(sentences) < 1:
             return [0]*5
 
-        avg_sentence_length = len(words) / len(sentences)
+        avg_sentence_length = len(words) / max(1, len(sentences))
         avg_word_length = sum(len(w) for w in words) / len(words)
         ttr = len(set(words)) / len(words)
 
@@ -52,7 +58,8 @@ def extract_features(text):
             "however", "therefore", "moreover", "furthermore", "thus",
             "in conclusion", "for example", "on the other hand"
         ]
-        connective_ratio = sum(text.lower().count(c) for c in connectors) / len(sentences)
+        connective_ratio = sum(text.lower().count(c) for c in connectors) / max(1, len(sentences))
+
         punctuation_density = len(re.findall(r"[,.!?;:]", text)) / len(words)
 
         return [
@@ -62,8 +69,11 @@ def extract_features(text):
             connective_ratio,
             punctuation_density
         ]
-    except:
+
+    except Exception as e:
+        print("FEATURE ERROR:", e)
         return [0]*5
+
 
 @app.post("/predict")
 def predict(data: InputText):
@@ -88,14 +98,17 @@ def predict(data: InputText):
     # Predicción y probabilidades
     pred = model.predict(scaled)[0]
     probs = model.predict_proba(scaled)[0]
+
+    probs_dict = dict(zip(model.classes_, probs))
+
     print("PRED:", pred)
-    print("PROBS:", probs)
+    print("PROBS:", probs_dict)
 
     return {
-        "prediction": pred,
-        "label": "🤖 IA" if pred == "ai" else "🧑‍🎓 Estudiante",
-        "probabilities": {
-            "student": round(probs[0] * 100, 2),
-            "ai": round(probs[1] * 100, 2)
-        }
+    "prediction": pred,
+    "probabilities": {
+        "student": round(probs_dict["student"] * 100, 2),
+        "ai": round(probs_dict["ai"] * 100, 2)
     }
+}
+
